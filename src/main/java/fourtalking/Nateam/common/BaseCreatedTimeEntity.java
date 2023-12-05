a1d78a5cbf6aeb5af4ace7ba3d374b32e1e5ee36
mistarson <yeowuli2@naver.com>
2023-12-05 19:59:47 +0900

    Fix: 생성 날짜와 수정 날짜 Auditing을 분
    생성 날짜만 필요한 엔티티의 경우를 생각해서 두 개를 분리

--- src/main/java/fourtalking/Nateam/common/BaseCreatedTimeEntity.java
+++ src/main/java/fourtalking/Nateam/common/BaseCreatedTimeEntity.java
@@ -6,18 +6,14 @@ import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(value = {AuditingEntityListener.class})
-public class BaseTimeEntity {
+public class BaseCreatedTimeEntity {
 
     @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdTime;
-
-    @LastModifiedDate
-    private LocalDateTime updatedTime;
 }
---
+++ src/main/java/fourtalking/Nateam/common/BaseLastModifiedTimeEntity.java
@@ -0,0 +1,17 @@
+package fourtalking.Nateam.common;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+public class BaseLastModifiedTimeEntity extends BaseCreatedTimeEntity{
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedTime;
+}
