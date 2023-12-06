6862fd6580d8f8f4aca1cb8d293c378c770518d9
mistarson <yeowuli2@naver.com>
2023-12-06 17:55:46 +0900

    Feature: 게임 단건 조회 API 기능 구현
    추후에 해야할 일
    1. userservice를 주입받아 userId를 이용해 userName 받아오기
    2. reviewService를 주입받아 각 리뷰의 평점을 계산해 게임의 전체 평점 추출하는 로직 구현하기

---
+++ src/main/java/fourtalking/Nateam/game/dto/GameGetDTO.java
@@ -0,0 +1,32 @@
+package fourtalking.Nateam.game.dto;
+
+import fourtalking.Nateam.game.entity.Game;
+import java.time.LocalDateTime;
+import lombok.Builder;
+
+@Builder
+public record GameGetDTO(
+        Long gameId,
+        String gameName,
+        String gameIntroduction,
+        int gamePrice,
+        double gameReviewRank,
+        String userName,
+        LocalDateTime createdDate,
+        LocalDateTime lastModifiedDate
+) {
+
+    public static GameGetDTO of(Game game, double gameReviewRank, String userName) {
+        return GameGetDTO.builder()
+                .gameId(game.getGameId())
+                .gameName(game.getGameName())
+                .gameIntroduction(game.getGameName())
+                .gamePrice(game.getGamePrice())
+                .gameReviewRank(gameReviewRank)
+                .userName(userName)
+                .createdDate(game.getCreatedTime())
+                .lastModifiedDate(game.getLastModifiedTime())
+                .build();
+    }
+
+}
--- src/main/java/fourtalking/Nateam/game/service/GameService.java
+++ src/main/java/fourtalking/Nateam/game/service/GameService.java
@@ -1,5 +1,6 @@
 package fourtalking.Nateam.game.service;
 
+import fourtalking.Nateam.game.dto.GameGetDTO;
 import fourtalking.Nateam.game.dto.GameRegisterDTO;
 import fourtalking.Nateam.game.entity.Game;
 import fourtalking.Nateam.game.repositroy.GameRepository;
@@ -14,14 +15,22 @@ public class GameService {
     private final GameRepository gameRepository;
 
     public Game findById(Long gameId) {
+
         return gameRepository.findById(gameId).orElseThrow(GameNotFoundException::new);
     }
 
     public GameRegisterDTO.Response registerGame(GameRegisterDTO.Request gameRequestDTO) {
+
         Game savedGame = gameRepository.save(gameRequestDTO.toEntity());
         double gameReviewRank = 5.00;
 
         return GameRegisterDTO.Response.of(savedGame, gameReviewRank);
     }
 
+    public GameGetDTO getGameById(Long gameId) {
+
+        Game game = gameRepository.findById(gameId).orElseThrow(GameNotFoundException::new);
+
+        return GameGetDTO.of(game, 4.1, "손창현");
+    }
 }
