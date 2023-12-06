dd90db83ba7003c91b230ddb0c28e32d9c2642d5
mistarson <yeowuli2@naver.com>
2023-12-07 02:47:52 +0900

    Feature: 게임 전체 목록 조회 API 기능 구현
    최신순으로 게임 전체 목록 조회 API 구현, 추후에 페이징 적용

--- src/main/java/fourtalking/Nateam/game/controller/GameController.java
+++ src/main/java/fourtalking/Nateam/game/controller/GameController.java
@@ -5,6 +5,7 @@ import fourtalking.Nateam.game.dto.GameRegisterDTO;
 import fourtalking.Nateam.game.dto.GameRegisterDTO.Response;
 import fourtalking.Nateam.game.service.GameService;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,6 +35,14 @@ public class GameController {
         GameGetDTO gameGetDTO = gameService.getGameById(gameId);
 
         return ResponseEntity.ok(gameGetDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getGames() {
+
+        List<GameGetDTO> gameGetDTOs = gameService.gameGetDTOs();
+
+        return ResponseEntity.ok(gameGetDTOs);
     }
 
 }
--- src/main/java/fourtalking/Nateam/game/repositroy/GameRepository.java
+++ src/main/java/fourtalking/Nateam/game/repositroy/GameRepository.java
@@ -1,8 +1,22 @@
 package fourtalking.Nateam.game.repositroy;
 
+import fourtalking.Nateam.game.dto.GameGetDTO;
 import fourtalking.Nateam.game.entity.Game;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
+
+    @Query("select "
+            + "new fourtalking.Nateam.game.dto.GameGetDTO("
+            + "g.gameId, g.gameName, g.gameIntroduction, g.gamePrice, "
+            + "avg(r.reviewRank), u.userName, g.createdTime, g.lastModifiedTime) "
+            + "from Game g "
+            + "left outer join Review r on g.gameId = r.gameId "
+            + "inner join User u on g.userId = u.userId "
+            + "group by g.gameId "
+            + "order by g.createdTime desc")
+    List<GameGetDTO> findAllGameGetDTOOrderByCreatedTime();
 
 }
