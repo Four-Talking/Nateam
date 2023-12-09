9d3ac11e5c6162eb79055299d8cb72854a296da7
mistarson <yeowuli2@naver.com>
2023-12-09 23:19:23 +0900

    Refactor: UserRole의 EnumType을 ORDINAL -> STRING으로 변경

--- src/main/java/fourtalking/Nateam/user/entity/User.java
+++ src/main/java/fourtalking/Nateam/user/entity/User.java
@@ -3,6 +3,8 @@ package fourtalking.Nateam.user.entity;
 import fourtalking.Nateam.user.constant.UserRole;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -38,6 +40,7 @@ public class User {
     private String userIntroduce;
 
     @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private UserRole userRole;
 
     public void editProfile(String nickname, String userIntroduce) {
